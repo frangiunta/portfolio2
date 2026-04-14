094adfbc3878f27a27e8bfcd6a182195bf50420f
frangiunta <98899679+frangiunta@users.noreply.github.com>
2026-04-14 19:44:58 +0000

    implementacion de glass en componentes

--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -9,11 +9,6 @@ import { EducacionComponent } from '../educacion/educacion';
 import { FooterComponent } from '../footer/footer';
 import { SkillsComponent } from "../skills/skills";
 import { ProyectosComponent } from "../proyectos/proyectos";
-<<<<<<< HEAD
-import { BackgroundComponent } from '../background/background';
-// Nota: Importa Skills y Proyectos cuando los tengamos listos
-=======
->>>>>>> 411c734b2d5642f3411def0c59478240fd07650f
 
 @Component({
   selector: 'app-main',
@@ -27,12 +22,8 @@ import { BackgroundComponent } from '../background/background';
     FooterComponent,
     SkillsComponent,
     ProyectosComponent,
-<<<<<<< HEAD
-    BackgroundComponent
-=======
     BackgroundComponent,
     App
->>>>>>> 9ebe28a2323de2937c784b0598b447a6002e03aa
 ],
   templateUrl: './main.html',
   styleUrls: ['./main.css']
