39c1b36d62ae4eb548e0fec65d5f1747c37aee71
frangiunta <98899679+frangiunta@users.noreply.github.com>
2026-05-04 01:04:28 -0300

    Refactor skills management: update skills interface, enhance skills display with descriptions, and improve modal backdrop styling

--- src/app/interface/skills.ts
+++ src/app/interface/skills.ts
@@ -1,6 +1,6 @@
 export interface Skills {
   id?: number;          // Opcional para la creación
   nombre: string;
-  porcentaje: number;   // Cambiado a number para cálculos y barras de progreso
-  skillCode: string;    // Tu identificador interno
+  descripcion: string;  
+  skillCode: string; 
 }
